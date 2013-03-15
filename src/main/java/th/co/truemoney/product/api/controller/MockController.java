78874eafae4db8dd6b7783392f7e7698bf7b45c7
Apinya Ukachoke <airske05@gmail.com>
2013-03-15 17:37:42 +0700

    Add mock data for 3310

---
+++ src/main/java/th/co/truemoney/product/api/controller/MockController.java
@@ -0,0 +1,188 @@
+package th.co.truemoney.product.api.controller;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import net.minidev.json.JSONObject;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import th.co.truemoney.product.api.util.ResponseParameter;
+
+public class MockController {
+
+	@RequestMapping(value = "/add-money/ewallet/banks", method = RequestMethod.GET)
+	public @ResponseBody
+	Map<String, Object> getDirectDebitInformation() {
+
+		JSONObject response = new JSONObject();
+		JSONObject data = new JSONObject();
+		JSONObject result = new JSONObject();
+
+		List<Map<String, Object>> listOfBank = new ArrayList<Map<String, Object>>();
+		Map<String, Object> scb = new HashMap<String,Object>();
+		scb.put("bankCode", "SCB");
+		scb.put("bankName", "ไทยพาณิชย์");
+		scb.put("bankNumber", "XXXX7890");
+		scb.put("min", "30");
+		scb.put("max", "3000");
+		scb.put("urlLogo",
+				"https://secure.truemoney-dev.com/m/tmn_webview/images/normal/Bank-SCB-2.png");
+		listOfBank.add(scb);
+
+		Map<String, Object> bbl = new HashMap<String,Object>();
+		bbl.put("bankCode", "BBL");
+		bbl.put("bankName", "กรุงเทพ");
+		bbl.put("bankNumber", "XXXX7891");
+		bbl.put("min", "30");
+		bbl.put("max", "30000");
+		bbl.put("urlLogo",
+				"https://secure.truemoney-dev.com/m/tmn_webview/images/normal/Bank-bk-2.png");
+		listOfBank.add(bbl);
+
+		Map<String, Object> ktb = new HashMap<String,Object>();
+		ktb.put("bankCode", "KTB");
+		ktb.put("bankName", "กรุงไทย");
+		ktb.put("bankNumber", "XXXX7892");
+		ktb.put("min", "30");
+		ktb.put("max", "30000");
+		ktb.put("urlLogo",
+				"https://secure.truemoney-dev.com/m/tmn_webview/images/normal/Bank-ktb-2.png");
+		listOfBank.add(ktb);
+
+		Map<String, Object> bay = new HashMap<String,Object>();
+		bay.put("bankCode", "BAY");
+		bay.put("bankName", "กรุงศรีอยุธยา");
+		bay.put("bankNumber", "XXXX7893");
+		bay.put("min", "30");
+		bay.put("max", "3000");
+		bay.put("urlLogo",
+				"https://secure.truemoney-dev.com/m/tmn_webview/images/normal/Bank-ks-2.png");
+		listOfBank.add(bay);
+
+		result.put("listOfBank", listOfBank);
+
+		data.put("data", result);
+
+		response.put(ResponseParameter.STATUS, "20000");
+		response.put(ResponseParameter.NAMESPACE, "TMN-PRODUCT");
+		response.put("data", data);
+		return response;
+
+	}
+
+	@RequestMapping(value = "/add-money/verify/ewallet", method = RequestMethod.POST)
+	public @ResponseBody
+	Map<String, Object> verifyEwallet(
+			@RequestParam(value = "amount") Double amount,
+			@RequestParam(value = "bankCode") String bankCode,
+			@RequestParam(value = "token") String token) {
+		Map<String, Object> data = new HashMap<String,Object>();
+		if ("SCB".equals(bankCode)) {
+			data.put("urlLogo",
+					"https://secure.truemoney-dev.com/m/tmn_webview/images/normal/Bank-SCB-2.png");
+			data.put("bankNameTH", "ไทยพาณิชย์");
+			data.put("bankNameEN", "scb");
+		} else if ("KTB".equals(bankCode)) {
+			data.put("urlLogo",
+					"https://secure.truemoney-dev.com/m/tmn_webview/images/normal/Bank-ktb-2.png");
+			data.put("bankNameTH", "กรุงไทย");
+			data.put("bankNameEN", "ktb");
+		} else if ("BBL".equals(bankCode)) {
+			data.put("urlLogo",
+					"https://secure.truemoney-dev.com/m/tmn_webview/images/normal/Bank-bk-2.png");
+			data.put("bankNameTH", "กรุงเทพ");
+			data.put("bankNameEN", "bbl");
+		} else if ("BAY".equals(bankCode)) {
+			data.put("urlLogo",
+					"https://secure.truemoney-dev.com/m/tmn_webview/images/normal/Bank-ks-2.png");
+			data.put("bankNameTH", "กรุงศรีอยุธยา");
+			data.put("bankNameEN", "bay");
+		}
+
+		data.put("bankCode", bankCode);
+		data.put("bankNumber", "XXXX7890");
+		data.put("fee", 1.00);
+		data.put("amount", amount);
+		data.put("transactionID", "1234567890");
+
+		JSONObject response = new JSONObject();
+
+		response.put(ResponseParameter.STATUS, "20000");
+		response.put(ResponseParameter.NAMESPACE, "TMN-PRODUCT");
+		response.put("data", data);
+		return response;
+
+	}
+
+	@RequestMapping(value = "/add-money/send/otp/{transactionID}/{accessToken}", method = RequestMethod.POST)
+	public @ResponseBody
+	Map<String, Object> sendOTP(
+			@PathVariable(value = "transactionID") String transactionID,
+			@RequestParam(value = "accessToken") String token) {
+		Map<String, Object> data = new HashMap<String,Object>();
+
+		data.put("otp", "287492");
+		data.put("refCode", "VNBM");
+		data.put("amount", 1000.00);
+
+		JSONObject response = new JSONObject();
+
+		response.put(ResponseParameter.STATUS, "20000");
+		response.put(ResponseParameter.NAMESPACE, "TMN-PRODUCT");
+		response.put("data", data);
+		return response;
+
+	}
+
+	@RequestMapping(value = "/add-money/confirm/ewallet/{accessToken}", method = RequestMethod.PUT)
+	public @ResponseBody
+	Map<String, Object> confirmEwallet(
+			@RequestParam(value = "accessToken") String token) {
+		JSONObject response = new JSONObject();
+		Map<String, Object> data = new HashMap<String, Object>();
+		response.put(ResponseParameter.STATUS, "20000");
+		response.put(ResponseParameter.NAMESPACE, "TMN-PRODUCT");
+		response.put("data", data);
+		return response;
+
+	}
+
+	@RequestMapping(value = "/add-money/checkstatus/ewallet/{accessToken}", method = RequestMethod.PUT)
+	public @ResponseBody
+	Map<String, Object> checkStatus(
+			@RequestParam(value = "accessToken") String token) {
+		Map<String, Object> data = new HashMap<String, Object>();
+
+		data.put("urlLogo",
+				"https://secure.truemoney-dev.com/m/tmn_webview/images/normal/Bank-SCB-2.png");
+		data.put("bankNameTH", "ไทยพาณิชย์");
+		data.put("bankNameEN", "scb");
+		data.put("bankNumber", "XXXX9845");
+		Calendar currentDate = Calendar.getInstance();
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MMM/dd HH:mm");
+		String dateNow = formatter.format(currentDate.getTime());
+		data.put("timestamp", dateNow);
+		data.put("transactionID", "12323423423");
+		data.put("amount", 1000.00);
+		data.put("fee", 3.00);
+		data.put("currentBalance", 10000.00);
+
+		JSONObject response = new JSONObject();
+
+		response.put(ResponseParameter.STATUS, "20000");
+		response.put(ResponseParameter.NAMESPACE, "TMN-PRODUCT");
+		response.put("data", data);
+		return response;
+
+	}
+
+}

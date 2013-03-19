56dd5a7f47c7de285268793c58a632016a71b367
Apinya Ukachoke <airske05@gmail.com>
2013-03-19 15:25:52 +0700

    Fix MockController

--- src/main/java/th/co/truemoney/product/api/domain/TopupDirectDebitRequest.java
+++ src/main/java/th/co/truemoney/product/api/domain/TopupDirectDebitRequest.java
@@ -3,19 +3,19 @@ package th.co.truemoney.product.api.domain;
 import java.math.BigDecimal;
 
 public class TopupDirectDebitRequest {
-	
-	private String sourceOfFundId;
-	
+
+	private String sourceOfFundID;
+
 	private BigDecimal amount;
-	
+
 	private String checksum;
 
-	public String getSourceOfFundId() {
-		return sourceOfFundId;
+	public String getSourceOfFundID() {
+		return sourceOfFundID;
 	}
 
-	public void setSourceOfFundId(String sourceOfFundId) {
-		this.sourceOfFundId = sourceOfFundId;
+	public void setSourceOfFundID(String sourceOfFundID) {
+		this.sourceOfFundID = sourceOfFundID;
 	}
 
 	public BigDecimal getAmount() {
@@ -33,5 +33,5 @@ public class TopupDirectDebitRequest {
 	public void setChecksum(String checksum) {
 		this.checksum = checksum;
 	}
-	
+
 }

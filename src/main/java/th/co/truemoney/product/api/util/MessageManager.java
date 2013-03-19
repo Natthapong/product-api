f842111a7e4742f10221eb59f416d74987eaf4c4
Tavee Khunbida <taveek@gmail.com>
2013-03-19 21:52:27 +0700

    add get header message to message manager

--- src/main/java/th/co/truemoney/product/api/util/MessageManager.java
+++ src/main/java/th/co/truemoney/product/api/util/MessageManager.java
@@ -14,13 +14,31 @@ public class MessageManager implements MessageSourceAware {
 	@Autowired
 	private MessageSource messageSource;
 	
-	public static final String UNKNOWN_MESSAGE = "Unknown Message Error";
+	public static final String UNKNOWN_MESSAGE = "Unknown Message";
+	
+	public static final String UNKNOWN_HEADER  = "Unknown Header";
 	
 	private static final Locale EN = new Locale("en", "US");
 	private static final Locale TH = new Locale("th", "TH");
 	
 	public void setMessageSource(MessageSource messageSource) {
 		this.messageSource = messageSource;
+	}
+	
+	public String getHeaderTh(String key, Object[] params) {
+		return getHeader(key, params, TH);
+	}
+	
+	public String getHeaderEn(String key, Object[] params) {
+		return getHeader(key, params, EN);
+	}
+	
+	private String getHeader(String key, Object[] params, Locale loc) {
+		try {
+			return messageSource.getMessage(key, params, loc);
+		} catch (NoSuchMessageException e) {
+			return UNKNOWN_HEADER;
+		}
 	}
 	
 	public String getMessageTh(String namespace, String code){

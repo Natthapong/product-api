3e3ebb8d21af7c39e526380352451dc5059ee3de
Tavee Khunbida <taveek@gmail.com>
2013-03-20 13:24:35 +0700

    set UTF-8 encoding for Thai message

--- src/main/java/th/co/truemoney/product/api/config/WebConfig.java
+++ src/main/java/th/co/truemoney/product/api/config/WebConfig.java
@@ -49,6 +49,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     public MessageSource messageSource() {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
         messageSource.setBasename("messages");
+        messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
     }
 	

565c81ab96e225af1259b011b7f8f3ed7749e754
schalermthai <schalermthai@wotifgroup.com>
2013-03-29 01:42:03 +0700

    added support for changing hostname for different profiles

--- src/main/java/th/co/truemoney/product/api/controller/GenericController.java
+++ src/main/java/th/co/truemoney/product/api/controller/GenericController.java
@@ -2,15 +2,26 @@ package th.co.truemoney.product.api.controller;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class GenericController {
+
+	@Autowired @Qualifier("apiHost")
+	private String apiHost;
+
 	@RequestMapping(value = "/generic", method = RequestMethod.GET)
-	public String staticGenericUrl(HttpServletResponse response) {
+	public ModelAndView staticGenericUrl(HttpServletResponse response) {
 		response.setContentType("application/json;charset=UTF-8");
-    	return "forward:/WEB-INF/content/generic.json";
+		return new ModelAndView("forward:/WEB-INF/pages/generic.jsp").addObject("apiHost", apiHost);
+	}
+
+	public void setApiHost(String apiHost) {
+		this.apiHost = apiHost;
 	}
 }

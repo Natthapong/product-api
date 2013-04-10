46c339f3400544f3f8538def8e74aa6f2ab5e896
TMN-02 <tanathip.se@gmail.com>
2013-04-10 15:08:25 +0700

    format testing code

--- src/test/java/th/co/truemoney/product/api/controller/TestBillController.java
+++ src/test/java/th/co/truemoney/product/api/controller/TestBillController.java
@@ -10,15 +10,19 @@ import th.co.truemoney.serviceinventory.bill.domain.BillInvoice;
 import th.co.truemoney.serviceinventory.bill.domain.BillPaymentInfo;
 
 public class TestBillController extends BaseTestController {
-	
+
 	String verifyTransferURL = String.format(
 			"/billpay/draft-transaction/%s/send-otp/%s", "1111111111111",
 			"1111111111111");
-	
+
 	@Test
-	public void urlCreateBillInvoice() throws Exception{
-		when(this.billPaymentServiceMock.createBillInvoice(any(BillPaymentInfo.class),anyString())).thenReturn(new BillInvoice());
-		this.verifySuccess(this.doPOST(verifyTransferURL, new BillPaymentInfo()));
+	public void urlCreateBillInvoice() throws Exception {
+		when(
+				this.billPaymentServiceMock.createBillInvoice(
+						any(BillPaymentInfo.class), anyString())).thenReturn(
+				new BillInvoice());
+		this.verifySuccess(this
+				.doPOST(verifyTransferURL, new BillPaymentInfo()));
 	}
-	
+
 }

0f4a94f6afff702a7ca987237b000a25c27628da
Apinya Ukachoke <airske05@gmail.com>
2013-04-26 18:57:55 +0700

    add new test

--- src/main/java/th/co/truemoney/product/api/controller/MobileWalletActivityController.java
+++ src/main/java/th/co/truemoney/product/api/controller/MobileWalletActivityController.java
@@ -1,5 +1,6 @@
 package th.co.truemoney.product.api.controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,20 +18,28 @@ import th.co.truemoney.serviceinventory.ewallet.domain.Activity;
 
 @RequestMapping(value = "/profile/activities")
 @Controller
-public class MobileWalletActivityController extends BaseController{
-	
+public class MobileWalletActivityController extends BaseController {
+
 	@Autowired
 	ActivityService activityService;
-	
+
 	@RequestMapping(value = "/list/{accessTokenID}", method = RequestMethod.GET)
 	@ResponseBody
 	public ProductResponse getActivityList(@PathVariable String accessTokenID) {
-		List<Activity> activityList = activityService.getActivities(accessTokenID);
-		
-		Map<String, Object> data = new HashMap<String, Object>();
-		
-		
-		return this.responseFactory.createSuccessProductResonse(data);
+		List<Activity> activityList = activityService
+				.getActivities(accessTokenID);
+
+		List<Map<String, String>> result = new ArrayList<Map<String, String>>();
+		for (Activity activity : activityList) {
+			Map<String, String> data = new HashMap<String, String>();
+			//data.put("reportID", activity.getReportID());
+			result.add(data);
+		}
+
+		Map<String, Object> returnData = new HashMap<String, Object>();
+		returnData.put("activities", result);
+
+		return this.responseFactory.createSuccessProductResonse(returnData);
 	}
-	
+
 }
--- src/test/java/th/co/truemoney/product/api/controller/TestMobileWalletActivityController.java
+++ src/test/java/th/co/truemoney/product/api/controller/TestMobileWalletActivityController.java
@@ -1,7 +1,9 @@
 package th.co.truemoney.product.api.controller;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -11,26 +13,21 @@ import org.junit.Test;
 import th.co.truemoney.serviceinventory.ewallet.domain.Activity;
 import th.co.truemoney.serviceinventory.exception.ServiceInventoryException;
 
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 public class TestMobileWalletActivityController extends BaseTestController {
 
 	private static final String fakeAccessToken = "1111111111";
 	private static final String getActivityListURL = String.format(
 			"/profile/activities/list/%s", fakeAccessToken);
-	
+	private MobileWalletActivityController controller;
 
 	@Before
 	public void setUp() throws Exception {
+		controller = new MobileWalletActivityController();
 	}
 
+	@SuppressWarnings("unchecked")
 	@Test
 	public void getActivityListSuccess() throws Exception {
-//		List<Activity> activityList = new ArrayList<Activity>();
-		
 		when(this.activityServiceMock.getActivities(fakeAccessToken)).thenReturn(Collections.EMPTY_LIST);
 		this.verifySuccess(this.doGET(getActivityListURL));
 	}
@@ -41,4 +38,16 @@ public class TestMobileWalletActivityController extends BaseTestController {
 				.thenThrow(new ServiceInventoryException(400, "", "", ""));
 		this.verifyFailed(this.doGET(getActivityListURL));
 	}
+	
+	@Test
+	public void test(){
+		List<Activity> mockActivities = new ArrayList<Activity>();
+		mockActivities.add(new Activity());
+		
+		when(this.activityServiceMock.getActivities(fakeAccessToken)).thenReturn(mockActivities);
+		
+		List<Activity> activities = this.activityServiceMock.getActivities(fakeAccessToken);
+		assertEquals(1, activities.size());
+	}
+	
 }

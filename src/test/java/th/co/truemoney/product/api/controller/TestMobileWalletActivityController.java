bbed1bf51c3c374e5f8d5f1ede3689ddf50eb067
Apinya Ukachoke <airske05@gmail.com>
2013-04-30 14:36:52 +0700

    Delete duplicate test

--- src/test/java/th/co/truemoney/product/api/controller/TestMobileWalletActivityController.java
+++ src/test/java/th/co/truemoney/product/api/controller/TestMobileWalletActivityController.java
@@ -17,7 +17,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import th.co.truemoney.product.api.domain.ActivityViewItem;
 import th.co.truemoney.product.api.domain.ProductResponse;
 import th.co.truemoney.serviceinventory.ewallet.domain.Activity;
 import th.co.truemoney.serviceinventory.exception.ServiceInventoryException;
@@ -27,7 +26,6 @@ public class TestMobileWalletActivityController extends BaseTestController {
 	private static final String fakeAccessToken = "1111111111";
 	private static final String getActivityListURL = String.format(
 			"/profile/activities/list/%s", fakeAccessToken);
-	private static final String imagesURL = "https://secure.truemoney-dev.com/m/tmn_webview/logo_activity_type";
 	
 	@Autowired
 	MobileWalletActivityController controller;
@@ -65,116 +63,6 @@ public class TestMobileWalletActivityController extends BaseTestController {
 		List<Activity> activityList = (List<Activity>) data.get("activities");
 		
 		assertEquals(1,activityList.size());
-	}
-	
-	@SuppressWarnings("unchecked")
-	@Test
-	public void topupMobileSuccess() throws Exception{
-		List<Activity> list = createActivityStub("topup_mobile","");
-		when(
-				this.activityServiceMock.getActivities(
-						fakeAccessToken
-				)).thenReturn(list);
-		ProductResponse resp = this.controller.getActivityList(fakeAccessToken);
-		Map<String, Object> data = resp.getData();
-		List<ActivityViewItem> activityList = (List<ActivityViewItem>) data.get("activities");
-		assertEquals(imagesURL+"/topup_mobile.png", 
-				activityList.get(0).getLogoURL());
-		assertEquals("เติมเงินมือถือ", activityList.get(0).getText1Th());
-		assertEquals("10/03/13",activityList.get(0).getText2Th());
-	}
-	
-	@SuppressWarnings("unchecked")
-	@Test
-	public void billPaySuccess() throws Exception{
-		List<Activity> list = createActivityStub("billpay","");
-		when(
-				this.activityServiceMock.getActivities(
-						fakeAccessToken
-				)).thenReturn(list);
-		ProductResponse resp = this.controller.getActivityList(fakeAccessToken);
-		Map<String, Object> data = resp.getData();
-		List<ActivityViewItem> activityList = (List<ActivityViewItem>) data.get("activities");
-		assertEquals(imagesURL+"/billpay.png", activityList.get(0).getLogoURL());
-		assertEquals("จ่ายบิล", activityList.get(0).getText1Th());
-		assertEquals("10/03/13",activityList.get(0).getText2Th());
-	}
-	
-	@SuppressWarnings("unchecked")
-	@Test
-	public void bonusSuccess() throws Exception{
-		List<Activity> list = createActivityStub("bonus","");
-		when(
-				this.activityServiceMock.getActivities(
-						fakeAccessToken
-						)).thenReturn(list);
-		ProductResponse resp = this.controller.getActivityList(fakeAccessToken);
-		Map<String, Object> data = resp.getData();
-		List<ActivityViewItem> activityList = (List<ActivityViewItem>) data.get("activities");
-		assertEquals(imagesURL+"/bonus.png", activityList.get(0).getLogoURL());
-		assertEquals("โปรโมชั่น", activityList.get(0).getText1Th());
-		assertEquals("10/03/13",activityList.get(0).getText2Th());
-	}
-	
-	@SuppressWarnings("unchecked")
-	@Test
-	public void addMoneySuccess() throws Exception{
-		List<Activity> list = createActivityStub("add_money","");
-		when(
-				this.activityServiceMock.getActivities(
-						fakeAccessToken
-						)).thenReturn(list);
-		ProductResponse resp = this.controller.getActivityList(fakeAccessToken);
-		Map<String, Object> data = resp.getData();
-		List<ActivityViewItem> activityList = (List<ActivityViewItem>) data.get("activities");
-		assertEquals(imagesURL+"/add_money.png", activityList.get(0).getLogoURL());
-		assertEquals("เติมเงิน Wallet", activityList.get(0).getText1Th());
-		assertEquals("10/03/13",activityList.get(0).getText2Th());
-	}
-	
-	@SuppressWarnings("unchecked")
-	@Test
-	public void transferDebtorSuccess() throws Exception{
-		List<Activity> list = createActivityStub("debtor","");
-		when(
-				this.activityServiceMock.getActivities(
-						fakeAccessToken
-						)).thenReturn(list);
-		ProductResponse resp = this.controller.getActivityList(fakeAccessToken);
-		Map<String, Object> data = resp.getData();
-		List<ActivityViewItem> activityList = (List<ActivityViewItem>) data.get("activities");
-		assertEquals(imagesURL+"/transfer.png", activityList.get(0).getLogoURL());
-		assertEquals("โอนเงินระหว่าง Wallet", activityList.get(0).getText1Th());
-		assertEquals("10/03/13",activityList.get(0).getText2Th());
-	}
-	
-	@SuppressWarnings("unchecked")
-	@Test
-	public void transferCreatorSuccess() throws Exception{
-		List<Activity> list = createActivityStub("creditor","");
-		when(
-				this.activityServiceMock.getActivities(
-						fakeAccessToken
-						)).thenReturn(list);
-		ProductResponse resp = this.controller.getActivityList(fakeAccessToken);
-		Map<String, Object> data = resp.getData();
-		List<ActivityViewItem> activityList = (List<ActivityViewItem>) data.get("activities");
-		assertEquals(imagesURL+"/transfer.png", activityList.get(0).getLogoURL());
-		assertEquals("โอนเงิน Wallet", activityList.get(0).getText1Th());
-		assertEquals("10/03/13",activityList.get(0).getText2Th());
-	}
-	
-	@Test
-	public void mapActionMessageSuccess() throws Exception{
-		List<Activity> list = createActivityStub("","");
-		when(
-				this.activityServiceMock.getActivities(
-						fakeAccessToken
-						)).thenReturn(list);
-		ProductResponse resp = this.controller.getActivityList(fakeAccessToken);
-		Map<String, Object> data = resp.getData();
-		List<ActivityViewItem> activityList = (List<ActivityViewItem>) data.get("activities");
-		assertEquals("TrueMove H", activityList.get(0).getText3Th());
 	}
 	
 	private List<Activity> createActivityStub(String type, String action) throws ParseException{

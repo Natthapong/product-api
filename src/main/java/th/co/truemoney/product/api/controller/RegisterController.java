02a991de9bac646960c65d7a56962bdd0e9b0228
Jenkins <jenkins.v1@truemoney.co.th>
2013-05-11 11:47:45 +0700

    clean up class by extract methods

--- src/main/java/th/co/truemoney/product/api/controller/RegisterController.java
+++ src/main/java/th/co/truemoney/product/api/controller/RegisterController.java
@@ -23,113 +23,116 @@ import th.co.truemoney.serviceinventory.exception.ServiceInventoryException;
 @RequestMapping(value = "/ewallet")
 public class RegisterController extends BaseController {
 
-	@Autowired
-	TmnProfileService profileService;
+    @Autowired
+    TmnProfileService profileService;
 
-	@Autowired
-	UserActionController userActionController;
+    @Autowired
+    UserActionController userActionController;
 
-	@Autowired
-	SecurityManager securityManager;
+    @Autowired
+    SecurityManager securityManager;
 
-	@RequestMapping(value = "/profiles/validate-email", method = RequestMethod.POST)
-	@ResponseBody
-	public ProductResponse validateEmail(
-			@RequestBody Map<String, String> request)
-			throws ServiceInventoryException {
+    @RequestMapping(value = "/profiles/validate-email", method = RequestMethod.POST)
+    @ResponseBody
+    public ProductResponse validateEmail(
+            @RequestBody Map<String, String> request)
+            throws ServiceInventoryException {
 
-		String email = request.get("email");
-		if (!ValidateUtil.checkEmail(email)) {
-			throw new InvalidParameterException("40000");
-		}
-		String returnData = "";
-		try {
-			returnData = profileService.validateEmail(MOBILE_APP_CHANNEL_ID,
-					email);
-		} catch (ServiceInventoryException e) {
-			String errorcode = String.format("%s.%s", e.getErrorNamespace(),
-					e.getErrorCode());
-			if (errorcode.equals("next.18")) {
-				throw new ServiceInventoryException(400, "10000",
-						"Email is already in used.", "TMN-PRODUCT");
-			}
-			throw e;
-		}
+        String email = request.get("email");
+        if (!ValidateUtil.checkEmail(email)) {
+            throw new InvalidParameterException("40000");
+        }
+        String returnData = verifyEmail(email);
 
-		Map<String, Object> data = new HashMap<String, Object>();
-		data.put("email", returnData);
+        Map<String, Object> data = new HashMap<String, Object>();
+        data.put("email", returnData);
 
-		return this.responseFactory.createSuccessProductResonse(data);
-	}
+        return this.responseFactory.createSuccessProductResonse(data);
+    }
 
-	@RequestMapping(value = "/profiles", method = RequestMethod.POST)
-	@ResponseBody
-	public ProductResponse createProfile(
-			@RequestBody Map<String, String> request)
-			throws ServiceInventoryException {
-		String email = request.get("email");
+    @RequestMapping(value = "/profiles", method = RequestMethod.POST)
+    @ResponseBody
+    public ProductResponse createProfile(
+            @RequestBody Map<String, String> request)
+            throws ServiceInventoryException {
+        String email = request.get("email");
 
-		if (!ValidateUtil.checkEmail(email)) {
-			throw new InvalidParameterException("40000");
-		}
-		if (!ValidateUtil.checkMobileNumber(request.get("mobileNumber"))) {
-			throw new InvalidParameterException("40001");
-		}
+        if (!ValidateUtil.checkEmail(email)) {
+            throw new InvalidParameterException("40000");
+        }
+        if (!ValidateUtil.checkMobileNumber(request.get("mobileNumber"))) {
+            throw new InvalidParameterException("40001");
+        }
 
-		TmnProfile tmnProfile = new TmnProfile();
-		tmnProfile.setEmail(email);
-		tmnProfile.setFullname(request.get("fullname"));
-		tmnProfile.setMobileNumber(request.get("mobileNumber"));
-		String password = securityManager.encryptRSA(request.get("password"));
-		tmnProfile.setPassword(password);
-		tmnProfile.setThaiID(request.get("thaiID"));
+        TmnProfile tmnProfile = new TmnProfile();
+        tmnProfile.setEmail(email);
+        tmnProfile.setFullname(request.get("fullname"));
+        tmnProfile.setMobileNumber(request.get("mobileNumber"));
+        String password = securityManager.encryptRSA(request.get("password"));
+        tmnProfile.setPassword(password);
+        tmnProfile.setThaiID(request.get("thaiID"));
 
-		OTP returnData = new OTP();
+        OTP returnData = requestCreateProfile(tmnProfile);
 
-		try {
-			returnData = profileService.createProfile(MOBILE_APP_CHANNEL_ID,
-					tmnProfile);
-		} catch (ServiceInventoryException e) {
-			String errorcode = String.format("%s.%s", e.getErrorNamespace(),
-					e.getErrorCode());
-			if (errorcode.equals("next.18")) {
-				throw new ServiceInventoryException(400, "10001",
-						"Mobile number is already in used.", "TMN-PRODUCT");
-			}
-			throw e;
-		}
+        Map<String, Object> data = new HashMap<String, Object>();
+        data.put("otpRefCode", returnData.getReferenceCode());
+        data.put("mobileNumber", returnData.getMobileNumber());
 
-		Map<String, Object> data = new HashMap<String, Object>();
-		data.put("otpRefCode", returnData.getReferenceCode());
-		data.put("mobileNumber", returnData.getMobileNumber());
+        return this.responseFactory.createSuccessProductResonse(data);
+    }
 
-		return this.responseFactory.createSuccessProductResonse(data);
-	}
+    @RequestMapping(value = "/profiles/verify-otp", method = RequestMethod.POST)
+    @ResponseBody
+    public ProductResponse confirmCreateProfile(@RequestBody Map<String, String> request)
+            throws ServiceInventoryException {
 
-	@RequestMapping(value = "/profiles/verify-otp", method = RequestMethod.POST)
-	@ResponseBody
-	public ProductResponse confirmCreateProfile(
-			@RequestBody Map<String, String> request)
-			throws ServiceInventoryException {
+        OTP otp = new OTP();
+        otp.setOtpString(request.get("otpString"));
+        otp.setReferenceCode(request.get("otpRefCode"));
+        otp.setMobileNumber(request.get("mobileNumber"));
 
-		OTP otp = new OTP();
-		otp.setOtpString(request.get("otpString"));
-		otp.setReferenceCode(request.get("otpRefCode"));
-		otp.setMobileNumber(request.get("mobileNumber"));
+        confirmCreateProfile(otp);
+        Map<String, Object> data = new HashMap<String, Object>();
 
-		try {
-			profileService.confirmCreateProfile(MOBILE_APP_CHANNEL_ID, otp);
-		} catch (ServiceInventoryException e) {
-			String errorcode = String.format("%s.%s", e.getErrorNamespace(),
-					e.getErrorCode());
-			if (!errorcode.equals("next.18")) {
-				throw e;
-			}
+        return this.responseFactory.createSuccessProductResonse(data);
+    }
 
-		}
-		Map<String, Object> data = new HashMap<String, Object>();
+    private String verifyEmail(String email) {
+        try {
+            return profileService.validateEmail(MOBILE_APP_CHANNEL_ID, email);
+        } catch (ServiceInventoryException e) {
+            String errorcode = String.format("%s.%s", e.getErrorNamespace(), e.getErrorCode());
+            if (errorcode.equals("next.18")) {
+                throw new ServiceInventoryException(400, "10000", "Email is already in used.", "TMN-PRODUCT");
+            }
+            throw e;
+        }
+    }
 
-		return this.responseFactory.createSuccessProductResonse(data);
-	}
+    private OTP requestCreateProfile(TmnProfile tmnProfile) {
+
+        try {
+            return profileService.createProfile(MOBILE_APP_CHANNEL_ID, tmnProfile);
+        } catch (ServiceInventoryException e) {
+            String errorcode = String.format("%s.%s", e.getErrorNamespace(), e.getErrorCode());
+            if (errorcode.equals("next.18")) {
+                throw new ServiceInventoryException(400, "10001",
+                        "Mobile number is already in used.", "TMN-PRODUCT");
+            }
+            throw e;
+        }
+    }
+
+    private void confirmCreateProfile(OTP otp) {
+        try {
+            profileService.confirmCreateProfile(MOBILE_APP_CHANNEL_ID, otp);
+        } catch (ServiceInventoryException e) {
+            String errorcode = String.format("%s.%s", e.getErrorNamespace(), e.getErrorCode());
+            if (!errorcode.equals("next.18")) {
+                throw e;
+            }
+
+        }
+    }
 
 }

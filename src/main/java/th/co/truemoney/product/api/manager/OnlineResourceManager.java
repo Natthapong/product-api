63110778bc071bca1402395f9e537684cfa330f9
Tavee Khunbida <taveek@gmail.com>
2013-05-15 13:53:45 +0700

    refactor OnlineResourceManager

--- src/main/java/th/co/truemoney/product/api/manager/OnlineResourceManager.java
+++ src/main/java/th/co/truemoney/product/api/manager/OnlineResourceManager.java
@@ -5,42 +5,33 @@ import org.springframework.stereotype.Component;
 @Component
 public class OnlineResourceManager {
 
-    private static final String logoBillURL = "https://secure.truemoney-dev.com/m/tmn_webview/images/logo_bill/";
+    private static final String LOGO_BILL_URL = "https://secure.truemoney-dev.com/m/tmn_webview/images/logo_bill/";
 
-    private static final String logoBankURL = "https://secure.truemoney-dev.com/m/tmn_webview/images/logo_bank/";
+    private static final String LOGO_BANK_URL = "https://secure.truemoney-dev.com/m/tmn_webview/images/logo_bank/";
 
-    private static final String logoActivityTypeURL = "https://secure.truemoney-dev.com/m/tmn_webview/images/logo_activity_type/";
+    private static final String LOGO_ACTIVITY_TYPE_URL = "https://secure.truemoney-dev.com/m/tmn_webview/images/logo_activity_type/";
 
     public String getBankLogoURL(String bankCode) {
-        return logoBankURL + lower(bankCode) + "@2x.png";
-    }
-
-    private String lower(String str) {
-        return str != null ? str.toLowerCase() : "";
+    	return LOGO_BANK_URL + lower(bankCode) + "@2x.png";
     }
 
     public String getActivityTypeLogoURL(String type) {
-        return logoActivityTypeURL + lower(type) + ".png";
+        return LOGO_ACTIVITY_TYPE_URL + removeSuffix(lower(type)) + ".png";
     }
 
     public String getActivityActionLogoURL(String action) {
-        if (action == null) {
-            return "";
-        }
         
-        action = removeSuffix(action);
+        action = removeSuffix(lower(action));
+        action = "tmvhtopup".equals(action) ? "tmvh" : action;
+        action = "tmvtopup".equals(action)  ? "trmv" : action;
         
-        String logoName = "";
-        if ("tmvhtopup".equals(action)) {
-            logoName = "tmvh";
-        } else if ("tmvtopup".equals(action)) {
-            logoName = "trmv";
-        } else {
-            logoName = action;
-        }
-        return logoBillURL + logoName + "@2x.png";
+        return LOGO_BILL_URL + action + "@2x.png";
      }
     
+    private String lower(String str) {
+    	return str != null ? str.toLowerCase() : "";
+    }
+
     private String removeSuffix(String s) {
     	return s.replace("_c", "");
     }

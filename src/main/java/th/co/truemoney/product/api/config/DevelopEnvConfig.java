a9317f4e0b385068952cd247e3f664de3bd8e5b6
Tavee Khunbida <taveek@gmail.com>
2013-05-21 13:35:15 +0700

    web.xml active spring profile configuration in pom.xml

--- src/main/java/th/co/truemoney/product/api/config/DevelopEnvConfig.java
+++ src/main/java/th/co/truemoney/product/api/config/DevelopEnvConfig.java
@@ -6,8 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("dev")
-public class DevEnvConfig {
+@Profile("develop")
+public class DevelopEnvConfig {
 
 	@Bean @Qualifier("apiHost")
 	public String apiHost() {

7b8780dcff77eeaf7767fe360f6e43e7e49ca23d
Apinya Ukachoke <airske05@gmail.com>
2013-05-21 11:24:17 +0700

    add ProductionEnvConfig

---
+++ src/main/java/th/co/truemoney/product/api/config/ProductionEnvConfig.java
@@ -0,0 +1,16 @@
+package th.co.truemoney.product.api.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class ProductionEnvConfig {
+	
+	@Bean @Qualifier("apiHost")
+	public String apiHost() {
+		return "https://secure.truemoney-dev.com"; //TODO
+	}
+}

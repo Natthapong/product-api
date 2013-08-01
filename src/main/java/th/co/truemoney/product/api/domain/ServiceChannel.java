fb03115830ecf94e4a1edc3273dc97c3d3334a7b
Tavee Khunbida <taveek@gmail.com>
2013-08-01 17:15:53 +0700

    refactoring section2 of topup ewallet activity details

--- src/main/java/th/co/truemoney/product/api/domain/ServiceChannel.java
+++ src/main/java/th/co/truemoney/product/api/domain/ServiceChannel.java
@@ -3,28 +3,35 @@ package th.co.truemoney.product.api.domain;
 import org.apache.commons.lang.NotImplementedException;
 
 public enum ServiceChannel {
-	CHANNEL_TMX(39, "True Money Express", "จุดบริการทรูมันนี่"),
-	CHANNEL_TRM(44, "True Shop", "ทรูช้อป"),
-	CHANNEL_ATM(42, "ATM", "เอทีเอ็ม"),
-	CHANNEL_KIOSK(33, "True Money Kiosk", "ตู้ทรูมันนี่"),
-	CHANNEL_MOBILE(40, "Bank account", "บัญชีธนาคาร"),
-	CHANNEL_IBANKING(43, "iBanking", "ไอแบงก์กิ้ง"),
-	CHANNEL_CP_FRESHMART(38, "CP Fresh Mart", "ซีพี เฟรชมาร์ท");
+	CHANNEL_TMX(39, "tmx", "True Money Express", "จุดบริการทรูมันนี่"),
+	CHANNEL_TRM(44, "trm", "True Shop", "ทรูช้อป"),
+	CHANNEL_ATM(42, "atm", "ATM", "เอทีเอ็ม"),
+	CHANNEL_KIOSK(33, "ksk", "True Money Kiosk", "ตู้ทรูมันนี่"),
+	CHANNEL_MOBILE(40, "mbl", "Bank account", "บัญชีธนาคาร"),
+	CHANNEL_IBANKING(43, "ibk", "iBanking", "ไอแบงก์กิ้ง"),
+	CHANNEL_CP_FRESHMART(38, "cpf", "CP Fresh Mart", "ซีพี เฟรชมาร์ท");
 	
 	private Integer id;
+	
+	private String abbre;
 	
 	private String nameEn;
 	
 	private String nameTh;
 	
-	private ServiceChannel(Integer id, String nameEn, String nameTh) {
+	private ServiceChannel(Integer id, String abbre, String nameEn, String nameTh) {
 		this.id = id;
+		this.abbre = abbre;
 		this.nameEn = nameEn;
 		this.nameTh = nameTh;
 	}
 	
 	public Integer getId() {
 		return id;
+	}
+
+	public String getAbbre() {
+		return abbre;
 	}
 
 	public String getNameEn() {
--- src/main/java/th/co/truemoney/product/api/handler/AddMoneyActivityDetailViewHandler.java
+++ src/main/java/th/co/truemoney/product/api/handler/AddMoneyActivityDetailViewHandler.java
@@ -22,6 +22,24 @@ public class AddMoneyActivityDetailViewHandler extends GeneralActivityDetailView
 	private static final Long IBANKING = 43l;
 	private static final Long TRM_CHANNEL = 44l;//
 	
+	@SuppressWarnings("serial")
+	static final Map<String , String> ref1TitleMap = new HashMap<String , String>() {{
+	    put("ksk_ref1_title_en", "Total amount");
+	    put("ksk_ref1_title_th", "ยอดเงินเข้า Wallet");
+	    put("cpf_ref1_title_en", "Store number");
+	    put("cpf_ref1_title_th", "หมายเลขสาขา");
+	    put("tmx_ref1_title_en", "Store number");
+	    put("tmx_ref1_title_th", "หมายเลขร้านค้า");
+	    put("mbl_ref1_title_en", "Bank");
+	    put("mbl_ref1_title_th", "ธนาคาร");
+	    put("atm_ref1_title_en", "Bank");
+	    put("atm_ref1_title_th", "ธนาคาร");
+	    put("ibk_ref1_title_en", "Bank");
+	    put("ibk_ref1_title_th", "ธนาคาร");
+	    put("trm_ref1_title_en", "Total amount");
+	    put("trm_ref1_title_th", "ยอดเงินเข้า Wallet");
+	}};
+	
 	@Override
 	public Map<String, String> buildSection1() {
 		Map<String, String> section1 = super.buildSection1();
@@ -46,95 +64,51 @@ public class AddMoneyActivityDetailViewHandler extends GeneralActivityDetailView
 		section1.put("titleTh", channelNameTh);
 		return section1;
 	}
-
+	
 	@Override
 	public Map<String, Object> buildSection2() {
 		Map<String, Object> section2 = super.buildSection2();
-		Long channel = activity.getChannel();
+		Map<String, Object> column1 = new HashMap<String, Object>();
+		
+		String ref1TitleEn = "";
+		String ref1TitleTh = "";
+		String ref1Value = "";
+		
 		String action  = activity.getAction();
 		if (SOF_TMCC.equals(action)) {
-			Map<String, Object> column1 = new HashMap<String, Object>();
-			Map<String, String> cell1 = new HashMap<String, String>();
-		 	cell1.put("titleTh", "รหัสบัตรเงินสด");
-			cell1.put("titleEn", "Cash Card PIN");
-			cell1.put("value", activity.getRef1());
-		 	column1.put("cell1", cell1);
-		 	section2.put("column1", column1);
-		} else if (KIOSK_CHANNEL.equals(channel)) {
-			Map<String, Object> column1 = new HashMap<String, Object>();
-			Map<String, String> cell1 = new HashMap<String, String>();
-			cell1.put("titleTh", "ยอดเงินเข้า Wallet");
-			cell1.put("titleEn", "Total amount");
-			cell1.put("value", Utils.formatAbsoluteAmount(activity.getTotalAmount()));
-			column1.put("cell1", cell1);
-			section2.put("column1", column1);	 	
-		} else if (CP_FRESH_MART_CHANNEL.equals(channel)) {
-			Map<String, Object> column1 = new HashMap<String, Object>();
-			Map<String, String> cell1 = new HashMap<String, String>();
-		 	cell1.put("titleTh", "หมายเลขสาขา");
-			cell1.put("titleEn", "Store number");
-			cell1.put("value", activity.getRef1());
-		 	column1.put("cell1", cell1);
-		 	section2.put("column1", column1);
-		} else if (TMX_CHANNEL.equals(channel)) {
-			Map<String, Object> column1 = new HashMap<String, Object>();
-			Map<String, String> cell1 = new HashMap<String, String>();
-		 	cell1.put("titleTh", "หมายเลขร้านค้า");
-			cell1.put("titleEn", "Store number");
-			cell1.put("value", activity.getRef1());
-		 	column1.put("cell1", cell1);
-		 	section2.put("column1", column1);
-		} else if (IOS_APP_CHANNEL.equals(channel)) {
-			Map<String, Object> column1 = new HashMap<String, Object>();
-			Map<String, String> cell1 = new HashMap<String, String>();
-			Map<String, String> cell2 = new HashMap<String, String>();
-		 	cell1.put("titleTh", "ธนาคาร");
-			cell1.put("titleEn", "Bank");
-			cell1.put("value", BankUtil.getThaiBankName(activity.getRef1()));
-			cell2.put("titleTh", "หมายเลขบัญชี");
-			cell2.put("titleEn", "Account number");
-			cell2.put("value", StringUtils.hasText(activity.getRef2()) ? activity.getRef2() : "-");
-			column1.put("cell2", cell2);
-		 	column1.put("cell1", cell1);
-		 	section2.put("column1", column1);
-		} else if (ATM_CHANNEL.equals(channel)) {
-			Map<String, Object> column1 = new HashMap<String, Object>();
-			Map<String, String> cell1 = new HashMap<String, String>();
-		 	cell1.put("titleTh", "ธนาคาร");
-			cell1.put("titleEn", "Bank");
-			cell1.put("value", BankUtil.getThaiBankName(activity.getRef1()));
-		 	column1.put("cell1", cell1);
-		 	section2.put("column1", column1);
-		} else if (IBANKING.equals(channel)) {
-			Map<String, Object> column1 = new HashMap<String, Object>();
-			Map<String, String> cell1 = new HashMap<String, String>();
-		 	cell1.put("titleTh", "ธนาคาร");
-			cell1.put("titleEn", "Bank");
-			cell1.put("value", BankUtil.getThaiBankName(activity.getRef1()));
-		 	column1.put("cell1", cell1);
-		 	section2.put("column1", column1);
-		} else if (TRM_CHANNEL.equals(channel)) {
-			Map<String, Object> column1 = new HashMap<String, Object>();
-			Map<String, String> cell1 = new HashMap<String, String>();
-			cell1.put("titleTh", "ยอดเงินเข้า Wallet");
-			cell1.put("titleEn", "Total amount");
-			cell1.put("value", Utils.formatAbsoluteAmount(activity.getTotalAmount()));
-			column1.put("cell1", cell1);
-			section2.put("column1", column1);	
+		 	ref1TitleTh = "รหัสบัตรเงินสด";
+			ref1TitleEn = "Cash Card PIN";
+			ref1Value = activity.getRef1();
 		} else {
-			Map<String, Object> column1 = new HashMap<String, Object>();
-			Map<String, String> cell1 = new HashMap<String, String>();
-			Map<String, String> cell2 = new HashMap<String, String>();
-		 	cell1.put("titleTh", "ธนาคาร");
-			cell1.put("titleEn", "Bank");
-			cell1.put("value", BankUtil.getThaiBankName(activity.getRef1()));
-			cell2.put("titleTh", "หมายเลขบัญชี");
-			cell2.put("titleEn", "Account number");
-			cell2.put("value", StringUtils.hasText(activity.getRef2()) ? activity.getRef2() : "-");
-			column1.put("cell2", cell2);
-		 	column1.put("cell1", cell1);
-		 	section2.put("column1", column1);
+			Long channelID = activity.getChannel();
+			ServiceChannel channel = ServiceChannel.getChannel(channelID.intValue());
+			ref1TitleEn = AddMoneyActivityDetailViewHandler.ref1TitleMap.get(channel.getAbbre() + "_ref1_title_en");
+			ref1TitleTh = AddMoneyActivityDetailViewHandler.ref1TitleMap.get(channel.getAbbre() + "_ref1_title_th");
+			ref1Value = activity.getRef1();
+			
+			if (ServiceChannel.CHANNEL_KIOSK == channel 
+					|| ServiceChannel.CHANNEL_TRM == channel) {
+				ref1Value = Utils.formatAbsoluteAmount(activity.getTotalAmount());
+			} else if (ServiceChannel.CHANNEL_ATM == channel 
+					|| ServiceChannel.CHANNEL_MOBILE == channel 
+					|| ServiceChannel.CHANNEL_IBANKING == channel) {
+				ref1Value = BankUtil.getThaiBankName(ref1Value);
+			}
+			
+			if (ServiceChannel.CHANNEL_MOBILE == channel) {
+				Map<String, String> cell2 = new HashMap<String, String>();
+				cell2.put("titleTh", "หมายเลขบัญชี");
+				cell2.put("titleEn", "Account number");
+				cell2.put("value", StringUtils.hasText(activity.getRef2()) ? activity.getRef2() : "-");
+				column1.put("cell2", cell2);
+			}
 		}
+		Map<String, String> cell1 = new HashMap<String, String>();
+		cell1.put("titleEn", ref1TitleEn);
+		cell1.put("titleTh", ref1TitleTh);
+		cell1.put("value", ref1Value);
+		column1.put("cell1", cell1);
+		section2.put("column1", column1);
 		return section2;
 	}
 

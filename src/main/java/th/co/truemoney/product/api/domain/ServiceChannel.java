fb3f6a8ee67752fa6f179571920c7b7fca31ee2c
Tavee Khunbida <taveek@gmail.com>
2013-08-02 14:52:52 +0700

    add more support channels

--- src/main/java/th/co/truemoney/product/api/domain/ServiceChannel.java
+++ src/main/java/th/co/truemoney/product/api/domain/ServiceChannel.java
@@ -3,19 +3,19 @@ package th.co.truemoney.product.api.domain;
 import org.apache.commons.lang.NotImplementedException;
 
 public enum ServiceChannel {
-	CHANNEL_TMX(39, "tmx", "True Money Express", "จุดบริการทรูมันนี่"),
-	CHANNEL_TRM(44, "trm", "True Shop", "ทรูช้อป"),
-	CHANNEL_ATM(42, "atm", "ATM", "เอทีเอ็ม"),
+	CHANNEL_SMS(1, "sms", "SMS", "SMS"),
+	CHANNEL_STK(2, "stk", "Sim Toolkit", "Sim Toolkit"),
+	CHANNEL_SOAP(4, "sop", "SOAP", "SOAP"),
+	CHANNEL_IVR(20, "ivr", "IVR", "IVR"),
+	CHANNEL_USSD(31, "usd", "USSD", "USSD"),
 	CHANNEL_KIOSK(33, "ksk", "True Money Kiosk", "ตู้ทรูมันนี่"),
+	CHANNEL_CP_FRESHMART(38, "cpf", "CP Fresh Mart", "ซีพี เฟรชมาร์ท"),
+	CHANNEL_TMX(39, "tmx", "True Money Express", "จุดบริการทรูมันนี่"),
+	CHANNEL_CPG(41, "cpg", "CPG", "CPG"),
 	CHANNEL_MOBILE(40, "mbl", "Bank account", "บัญชีธนาคาร"),
+	CHANNEL_ATM(42, "atm", "ATM", "เอทีเอ็ม"),
 	CHANNEL_IBANKING(43, "ibk", "iBanking", "ไอแบงก์กิ้ง"),
-	CHANNEL_CP_FRESHMART(38, "cpf", "CP Fresh Mart", "ซีพี เฟรชมาร์ท");
-	//CPG - 41
-	//USSD - 31
-	//STK - 2
-	//SMS - 1
-	//SOAP - 4
-	//IVR - 20
+	CHANNEL_TRM(44, "trm", "True Shop", "ทรูช้อป");
 	
 	private Integer id;
 	

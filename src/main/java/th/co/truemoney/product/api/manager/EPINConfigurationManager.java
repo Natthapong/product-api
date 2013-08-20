99d993204b2952c0839f77d4fb64db1564767d5f
Tavee Khunbida <taveek@gmail.com>
2013-08-20 10:16:45 +0700

    refactor ePIN configuration manager

--- src/main/java/th/co/truemoney/product/api/manager/EPINConfigurationManager.java
+++ src/main/java/th/co/truemoney/product/api/manager/EPINConfigurationManager.java
@@ -1,6 +1,7 @@
 package th.co.truemoney.product.api.manager;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,15 +19,18 @@ public class EPINConfigurationManager {
 	public List<String> getEpinPrice() {
 		if (priceList == null) {
 			String priceStr = env.getProperty("\"available.epin.prices\"");
-			priceStr = priceStr.trim();
-			priceStr = priceStr.replace("[", "");
-			priceStr = priceStr.replace("]", "");
-			priceStr = priceStr.replaceAll("\"", "");
-			priceStr = priceStr.replaceAll(" ", "");
-			System.out.println(priceStr);
-			priceList = Arrays.asList(priceStr.split(","));
+			if (priceStr == null) {
+				priceList = Collections.emptyList();
+			} else {
+				priceStr = priceStr.trim();
+				priceStr = priceStr.replaceAll("\\[", "")
+								   .replaceAll("\\]", "")
+								   .replaceAll("\"", "")
+								   .replaceAll(" ", "");
+				priceList = Arrays.asList(priceStr.split(","));
+			}
+			
 		}
 		return priceList;
 	}
-	
 }

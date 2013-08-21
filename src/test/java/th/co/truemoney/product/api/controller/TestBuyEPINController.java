bef4069fbb041902e50f582b93788105f5b08417
Pavares Sakunee <onieoni@gmail.com>
2013-08-21 11:33:39 +0700

    check null recipient mobile number

--- src/test/java/th/co/truemoney/product/api/controller/TestBuyEPINController.java
+++ src/test/java/th/co/truemoney/product/api/controller/TestBuyEPINController.java
@@ -3,22 +3,31 @@ package th.co.truemoney.product.api.controller;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import th.co.truemoney.serviceinventory.buy.domain.BuyProduct;
 import th.co.truemoney.serviceinventory.buy.domain.BuyProductDraft;
 import th.co.truemoney.serviceinventory.ewallet.domain.OTP;
+import th.co.truemoney.serviceinventory.exception.ServiceInventoryException;
+import th.co.truemoney.serviceinventory.topup.domain.TopUpMobileDraft;
+import th.co.truemoney.serviceinventory.buy.domain.BuyProductTransaction;
 
 public class TestBuyEPINController extends BaseTestController {
 
 	private static final String fakeAccessToken = "111111111111";
 	
 	private static final String createBuyEpinURL = String.format("/buy/e-pin/draft/verifyAndCreate/%s", fakeAccessToken);
+	
+	private static final String confirmOTPURL = String.format("/buy/e-pin/confirm/%s/%s", "1111111111", fakeAccessToken);
+	
+	private static final String checkStatusURL = String.format("/buy/e-pin/%s/status/%s", "1111111111", fakeAccessToken);
 	
 	@Test
 	public void createBuyEpinSuccess() throws Exception {
@@ -44,7 +53,7 @@ public class TestBuyEPINController extends BaseTestController {
         params.put("recipientMobileNumber", "0811111111");
         
         this.verifySuccess(this.doPOST(createBuyEpinURL, params));
-		
+        
 	}
 	
 	@Test
@@ -59,4 +68,73 @@ public class TestBuyEPINController extends BaseTestController {
 		
 	}
 	
+	@Test
+	public void integrationTestConfirmOTPSuccess() throws Exception {
+
+		when(
+				transactionAuthenServiceMock.verifyOTP(anyString(),
+						any(OTP.class), anyString())).thenReturn(
+				TopUpMobileDraft.Status.OTP_CONFIRMED);
+
+		Map<String, Object> reqBody = new HashMap<String, Object>();
+		reqBody.put("otpString", "123456");
+		reqBody.put("otpRefCode", "QWE");
+
+		this.verifySuccess(this.doPUT(confirmOTPURL, reqBody))
+				.andExpect(jsonPath("data").exists())
+				.andExpect(jsonPath("$..status").value("CONFIRMED"));
+
+		Mockito.verify(buyProductServiceMock).performBuyProduct(anyString(), anyString());
+	}
+	
+	@Test
+	public void confirmOTPFail() throws Exception {
+
+		when(
+				transactionAuthenServiceMock.verifyOTP(anyString(),
+						any(OTP.class), anyString())).thenThrow(
+				new ServiceInventoryException(400, "", "", "TMN-PRODUCT"));
+
+		Map<String, Object> reqBody = new HashMap<String, Object>();
+		reqBody.put("otpString", "123456");
+		reqBody.put("otpRefCode", "QWE");
+
+		this.verifyFailed(this.doPUT(confirmOTPURL, reqBody));
+
+		Mockito.verify(buyProductServiceMock, Mockito.never()).performBuyProduct(anyString(), anyString());
+
+	}
+	
+	@Test
+	public void integrationTestCheckStatusSuccess() throws Exception {
+		
+		when(
+				buyProductServiceMock.getBuyProductStatus(anyString(),
+						anyString())).thenReturn(
+				BuyProductTransaction.Status.SUCCESS);
+
+		Map<String, Object> reqBody = new HashMap<String, Object>();
+		reqBody.put("otpString", "123456");
+		reqBody.put("otpRefCode", "QWE");
+
+		this.verifySuccess(this.doGET(checkStatusURL, reqBody))
+				.andExpect(jsonPath("data").exists())
+				.andExpect(jsonPath("$..status").value("SUCCESS"));
+	}
+
+	@Test
+	public void checkStatusFail() throws Exception {
+
+		when(
+				buyProductServiceMock.getBuyProductStatus(anyString(),
+						anyString())).thenThrow(
+				new ServiceInventoryException(400, "", "", "TMN-PRODUCT"));
+
+		Map<String, Object> reqBody = new HashMap<String, Object>();
+		reqBody.put("otpString", "123456");
+		reqBody.put("otpRefCode", "QWE");
+
+		this.verifyFailed(this.doGET(checkStatusURL, reqBody));
+
+	}
 }

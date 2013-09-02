6572293b444ae41241d801ae54396a8192e17378
Tavee Khunbida <taveek@gmail.com>
2013-09-02 13:42:21 +0700

    add tvg to bill true list

--- src/main/java/th/co/truemoney/product/api/util/Utils.java
+++ src/main/java/th/co/truemoney/product/api/util/Utils.java
@@ -14,7 +14,7 @@ import th.co.truemoney.serviceinventory.bill.domain.SourceOfFund;
 
 public final class Utils {
 
-    private static List<String> trueCorpBills = Arrays.asList("catv", "dstv", "tcg", "ti", "tic", "tlp", "tmvh", "tr", "trmv", "true", "rft", "tmob");
+    private static List<String> trueCorpBills = Arrays.asList("catv", "dstv", "tcg", "ti", "tic", "tlp", "tmvh", "tr", "trmv", "true", "rft", "tmob", "tgv");
 
     public static Boolean isTrueCorpBill(String target) {
         return trueCorpBills.contains(target);
